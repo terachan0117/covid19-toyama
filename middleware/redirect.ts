6db47c3938028420ed34a29d9c11834bebe3c743
Issei Terada <58585239+terachan0117@users.noreply.github.com>
2023-10-15 21:20:07 +0900

    Set redirect

--- middleware/redirect.ts
+++ middleware/redirect.ts
@@ -1,11 +1,14 @@
 import { Middleware } from '@nuxt/types'
 
 const redirect: Middleware = ({ route, redirect }) => {
+  redirect(301, 'https://www.pref.toyama.jp/1021/kurashi/kenkou/iryou/virus/index.html')
+  /*
   if (route.path.includes('flow')) {
     redirect(
       'http://www.pref.toyama.jp/cms_sec/1205/kj00021473.html'
     )
   }
+  */
 }
 
 export default redirect
